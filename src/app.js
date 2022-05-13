301db7ef5d3b6b6ec9a827785c1a4494d2cdfc8d
isabel.sleigman <sleimusic@gmail.com>
2022-05-13 12:00:35 -0300

    buscando livros do banco

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./config/dbConnect.js";
+import livros from "./models/Livro.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 db.once("open", () => {
@@ -10,18 +11,15 @@ const app = express();
 
 app.use(express.json())
 
-const livros = [
-    { id: 1, "titulo": "Senhor dos Aneis" },
-    { id: 2, "titulo": "O Hobbit" }
-]
-
 app.get('/', (requisicao, resposta) => {
     resposta.status(200).send('Curso de node');
 })
 
 app.get('/livros', (requisicao, resposta) => {
-    resposta.status(200).json(livros);
-
+    livros.find((err, livros) => {
+        resposta.status(200).json(livros);
+    })
+ 
 })
 
 app.get('/livros/:id', (requisicao, resposta) => {
