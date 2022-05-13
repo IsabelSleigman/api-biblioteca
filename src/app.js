fdc9248eb21242f4d6f849613ad7b191fad20613
isabel.sleigman <sleimusic@gmail.com>
2022-05-13 13:48:28 -0300

    iniciando alterações no app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import livros from "./models/Livro.js";
+import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 db.once("open", () => {
@@ -11,16 +12,7 @@ const app = express();
 
 app.use(express.json())
 
-app.get('/', (requisicao, resposta) => {
-    resposta.status(200).send('Curso de node');
-})
-
-app.get('/livros', (requisicao, resposta) => {
-    livros.find((err, livros) => {
-        resposta.status(200).json(livros);
-    })
- 
-})
+routes(app)
 
 app.get('/livros/:id', (requisicao, resposta) => {
     let index = buscaLivro(requisicao.params.id);
